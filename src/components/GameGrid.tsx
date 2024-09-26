a7cbb3c31b97729bf90dc4192cbc1087427d586b
Lijuan Wang <2738042w@student.gla.ac.uk>
2024-09-26 14:14:15 +0100

    create infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,8 @@
-import { Box, Button, SimpleGrid, Text } from '@chakra-ui/react'
+import { Box, Button, SimpleGrid, Spinner, Text } from '@chakra-ui/react'
 import React from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import { GameQuery } from '../App'
 import useGames from '../react_query/hooks/useGames'
-import { Platform } from '../services/GameService'
-import { Genre } from '../services/GenreService'
 import GameCard from './GameCard'
 import GameCardContainer from './GameCardContainer'
 import GameCardSkeleton from './GameCardSkeleton'
@@ -16,11 +15,17 @@ const GameGrid = ({ gameQuery }: Props) => {
     const {data, error, isLoading, fetchNextPage, isFetchingNextPage, hasNextPage} = useGames(gameQuery)
     const skeletons = [1, 2, 3, 4, 5, 6]
 
+    const fetchedGamesCount = data?.pages.reduce((total, page) => total + page.results.length, 0) || 0
+
     if(error) return <Text>{error.message}</Text>
 
     return (
-        <Box padding='10px'>
-            <SimpleGrid columns={{sm:1, md:2, lg:3, xl:4}} spacing={6}>
+        <InfiniteScroll
+        dataLength={fetchedGamesCount}
+        hasMore={!!hasNextPage}
+        next={() => fetchNextPage()}
+        loader={<Spinner />}>
+            <SimpleGrid columns={{sm:1, md:2, lg:3, xl:4}} spacing={6} padding='10px'>
                 {isLoading && skeletons.map(
                     skeleton => 
                     <GameCardContainer key={skeleton}>
@@ -38,14 +43,7 @@ const GameGrid = ({ gameQuery }: Props) => {
                     </React.Fragment>
                 )}
             </SimpleGrid>
-            {hasNextPage && 
-            <Button 
-                disabled={isFetchingNextPage} 
-                marginY={5} 
-                onClick={() => fetchNextPage()}>
-                    {isFetchingNextPage ? 'loading' : 'Load More'}
-            </Button>}
-        </Box>
+        </InfiniteScroll>
             
     )
 }
